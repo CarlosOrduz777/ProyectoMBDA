daee62b69fbb5947801e372786abe4f941697f2e
Derjasai <elholaquehace88@gmail.com>
2021-10-09 20:40:45 -0500

    Agregacion de xtablas y x poblar junto a consultas y leves correciones en modelo General Extendido

---
+++ SQL/xPoblar.sql
@@ -0,0 +1,11 @@
+--xPoblar
+DELETE FROM Ordenes;
+DELETE FROM Productos;
+DELETE FROM Tiendas;
+DELETE FROM TiendasProductos;
+DELETE FROM Calificaciones;
+DELETE FROM Cupones;
+DELETE FROM Telefonos;
+DELETE FROM Clientes;
+DELETE FROM Direcciones;
+DELETE FROM Domiciliarios;
