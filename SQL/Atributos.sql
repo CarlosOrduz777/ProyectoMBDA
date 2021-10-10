7e47bf18b8303a4a636ed44797f0053b65d19ad7
Carlos Orduz <carlos.orduz@mail.escuelaing.edu.co>
2021-10-09 23:44:45 -0500

    Atributos terminados

---
+++ SQL/Atributos.sql
@@ -0,0 +1,14 @@
+--Atributos
+
+ALTER TABLE TiendasProductos ADD CONSTRAINT CK_PrecioProducto
+	CHECK (precio > 0);
+	
+ALTER TABLE Calificaciones ADD CONSTRAINT CK_valoracion
+	CHECK (valoracion <= 5 AND valoracion >= 1);
+	
+ALTER TABLE Ordenes ADD CONSTRAINT CK_MetodoPago
+	CHECK (metodoPago IN ('E','P','C','D'));
+	
+ALTER TABLE Ordenes ADD CONSTRAINT CK_Estado
+	CHECK (Estado IN ('E','L','F'));
+	
