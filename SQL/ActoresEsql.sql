069cb88557aacfc889522a267035f006b05717c8
Carlos Orduz <carlos.orduz@mail.escuelaing.edu.co>
2021-12-08 20:06:58 -0500

    avance 2.0

--- SQL/ActoresEsql.sql
+++ SQL/ActoresEsql.sql
@@ -2,7 +2,7 @@
 
 CREATE OR REPLACE PACKAGE PK_Cliente IS
 	PROCEDURE adOrden(xid NUMBER,xobservaciones varchar,xhoraInicio varchar,xhoraFinalizacion varchar,xestado varchar,xidDomiciliario number, xidCliente number);
-	PROCEDURE moOrden(xid number,xobservaciones varchar,xestado varchar);
+	PROCEDURE moOrden(xid NUMBER,xobservaciones varchar,xhoraInicio varchar,xhoraFinalizacion varchar,xestado varchar,xidDomiciliario number, xidCliente number);
 	PROCEDURE elOrden(xid number);
 END PK_Cliente;
 /
@@ -11,14 +11,29 @@ CREATE OR REPLACE PACKAGE PK_Domiliciario IS
 	PROCEDURE consultarOrden(xid number);
 END PK_Domiliciario;
 /
+CREATE OR REPLACE PACKAGE PK_Gerente IS
+	PROCEDURE adTienda(xnombre varchar, xdireccion varchar, xhoraApertura varchar,xhoraCierre varchar,xzona varchar,xdisponible varchar);
+	PROCEDURE moTienda(xnombre varchar, xdireccion varchar, xhoraApertura varchar,xhoraCierre varchar,xzona varchar,xdisponible varchar);
+	PROCEDURE elTienda(xnombre varchar,xdireccion varchar);
+	PROCEDURE coTienda(xnombre varchar,xdireccion varchar);
+	PROCEDURE adUsuario();
+	PROCEDURE elUsuario();
+	PROCEDURE coUsuario();
+END PK_Gerente;
+/
+
+CREATE OR REPLACE PACKAGE PK_Tienda IS
+	PROCEDURE adProducto();
+	PROCEDURE moProducto();
+	PROCEDURE elProducto();
+	PROCEDURE coProducto();
+END PK_Tienda;
+/
 
 
 
 
-
-
-
-CREATE ROLE PK_Ciente;
+CREATE ROLE PK_Cliente;
 
 CREATE ROLE PK_Domiliciario;
 
